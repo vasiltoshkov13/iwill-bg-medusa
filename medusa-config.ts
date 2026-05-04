15bffe1eb5974ed4f632370416be7c4f98525aec
Vasil Toshkov <vasiltoshkov@gmail.com>
2026-05-04 23:18:04 +0300

    Fix admin backend URL

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,7 +5,7 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   admin: {
     disable: false,
-    backendUrl: process.env.MEDUSA_BACKEND_URL || "https://api.iwill.bg",
+    backendUrl: process.env.MEDUSA_BACKEND_URL || "https://medusa-backend-production-bd55.up.railway.app",
   },
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
